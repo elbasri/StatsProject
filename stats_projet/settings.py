b992953f6f013e2a7a8dda5a13f05ea362d264d4
khaleddamou <150620682+khaleddamou@users.noreply.github.com>
2023-11-27 12:11:52 +0100

    updates

--- stats_projet/settings.py
+++ stats_projet/settings.py
@@ -80,12 +80,12 @@ DATABASES = {
     #'default': {
     #    'ENGINE': 'django.db.backends.sqlite3',
     #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
+    #} 
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'stats_projet',
-        'USER': 'stats_projet',
-        'PASSWORD': 'NCR123stats***',
+        'USER': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
